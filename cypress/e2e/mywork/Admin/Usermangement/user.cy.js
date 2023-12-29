36a1ab7964c69533e2e9720a15fbefba7dcac0f8
bharatiy <bharatiyedave139@gmail.com>
2023-12-29 19:02:52 +0530

    1234

--- cypress/e2e/mywork/Admin/Usermangement/user.cy.js
+++ cypress/e2e/mywork/Admin/Usermangement/user.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 import AdminUserManagement from "../../../../support/PageObjectModel/Admin/User/AdminUserManagement.cy";
 //////////////////////////////////////////////////////////////////////////////
-const adminUserObj = new AdminUserManagement();
+const adminUserObj =
+  new AdminUserManagement();
 describe("Validating User", () => {
   it.only("Vaidating User Url", () => {
     cy.login("Admin", "admin123");
@@ -11,9 +12,13 @@ describe("Validating User", () => {
   it.only("Validating filter of system user", () => {
     cy.login("Admin", "admin123");
     cy.admin();
-    adminUserObj.filterButton.should("be.visible");
+    adminUserObj.filterButton.should(
+      "be.visible",
+    );
     adminUserObj.filterTabClick.click();
-    adminUserObj.filterButton.should("not.be.visible");
+    adminUserObj.filterButton.should(
+      "not.be.visible",
+    );
   });
   it.only("Validating Username block", () => {
     cy.login("Admin", "admin123");
@@ -23,7 +28,11 @@ describe("Validating User", () => {
   it.only("Validating Userrole dropdown block", () => {
     cy.login("Admin", "admin123");
     cy.admin();
-    adminUserObj.userRole.find("div").find("i").eq(0).click();
+    adminUserObj.userRole
+      .find("div")
+      .find("i")
+      .eq(0)
+      .click();
     adminUserObj.userRolDropdown
       .find(".oxd-select-option")
       .contains("Admin")
@@ -38,28 +47,44 @@ describe("Validating User", () => {
       .find("div")
       .find("span")
       .then((resp) => {
-        cy.wrap(resp).contains("Krishna Kumar Pasupuleti").click();
+        cy.wrap(resp)
+          .contains(
+            "Krishna Kumar Pasupuleti",
+          )
+          .click();
       });
   });
   it.only("Validating Status box", () => {
     cy.login("Admin", "admin123");
     cy.admin();
-    adminUserObj.status.find("div").find("i").eq(1).click();
+    adminUserObj.status
+      .find("div")
+      .find("i")
+      .eq(1)
+      .click();
     adminUserObj.statusDropdown
       .find(".oxd-select-option")
       .contains("Enabled")
       .click();
   });
-
+  //commenting
   it("Validating User child module", () => {
     cy.login("Admin", "admin123");
     cy.admin();
     adminUserObj.filterTabClick.click();
-    adminUserObj.filterButton.should("not.be.visible");
+    adminUserObj.filterButton.should(
+      "not.be.visible",
+    );
     adminUserObj.filterTabClick.click();
-    adminUserObj.filterButton.should("be.visible");
+    adminUserObj.filterButton.should(
+      "be.visible",
+    );
     adminUserObj.userName.type("admin");
-    adminUserObj.userRole.find("div").find("i").eq(0).click();
+    adminUserObj.userRole
+      .find("div")
+      .find("i")
+      .eq(0)
+      .click();
     adminUserObj.userRolDropdown
       .find(".oxd-select-option")
       .contains("Admin")
@@ -69,9 +94,17 @@ describe("Validating User", () => {
       .find("div")
       .find("span")
       .then((resp) => {
-        cy.wrap(resp).contains("Krishna Kumar Pasupuleti").click();
+        cy.wrap(resp)
+          .contains(
+            "Krishna Kumar Pasupuleti",
+          )
+          .click();
       });
-    adminUserObj.status.find("div").find("i").eq(1).click();
+    adminUserObj.status
+      .find("div")
+      .find("i")
+      .eq(1)
+      .click();
     adminUserObj.statusDropdown
       .find(".oxd-select-option")
       .contains("Enabled")
